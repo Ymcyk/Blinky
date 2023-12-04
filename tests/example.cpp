28fba0c74b6bcedd831fd1e892690543d264a61b
Piotr Zieliński <piotrziel@outlook.com>
2023-12-04 23:37:44 +0100

    Example of working unit tests

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,6 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
+#include <fff.h>
 
 #include <cstdint>
+
+DEFINE_FFF_GLOBALS;
+
+void DISPLAY_init();
+
+FAKE_VOID_FUNC(DISPLAY_init);
 
 uint32_t factorial( uint32_t number ) {
     return number <= 1 ? number : factorial(number-1) * number;
@@ -11,4 +18,8 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial( 2) == 2 );
     REQUIRE( factorial( 3) == 6 );
     REQUIRE( factorial(10) == 3'628'800 );
+
+    DISPLAY_init();
+
+    REQUIRE( DISPLAY_init_fake.call_count == 1 );
 }
